a7b6db861b2bf5bae17b6e124a9c34ea0fafbe92
nicovidal <nico.vidal.m@gmail.com>
2023-08-19 16:05:09 -0400

    redux already install!

--- src/TaskApp.jsx
+++ src/TaskApp.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { TaskMainPage } from './TaskMainPage/page/TaskMainPage'
+import { Provider } from "react-redux"
+import { store } from "./store/store"
+
 
 export const TaskApp = () => {
   return (
     <>
+      <Provider store={store }>
       <TaskMainPage/>
+      </Provider>
     </>
   )
 }
