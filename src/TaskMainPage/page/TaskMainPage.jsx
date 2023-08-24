e05112b2d0591bcfeeeb27f7cedebe97eb2ef88e
nicovidal <nico.vidal.m@gmail.com>
2023-08-23 20:08:24 -0400

    filtro palabras ok

--- src/TaskMainPage/page/TaskMainPage.jsx
+++ src/TaskMainPage/page/TaskMainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { HeadDate } from "../components/headDate";
 import { TaskCard } from "../components/TaskCard";
 import { TaskFilter } from "../components/TaskFilter";
@@ -8,17 +8,37 @@ import { TaskModal } from "../components/TaskModal";
 import { useModal } from "../../hooks/useModal";
 import { useFilter } from "../../hooks/useFilter";
 
+
 export const TaskMainPage = () => {
   const { tasks, startLoadingTasks, setActiveTask } = useTaskStore();
   const { openModal } = useModal();
-
   const { setSelectedFilter, setSortOrder, filteredAndSortedTasks } = useFilter(tasks);
+  const [searchValue, setSearchValue] = useState("");
+  const [startDateFilter, setStartDateFilter] = useState(null);
+  const [endDateFilter, setEndDateFilter] = useState(null);
+
+  const filterTask = (task) => {
+    if (searchValue === "") {
+      return true;
+    } else {
+      const lowerCaseSearchValue = searchValue.toLowerCase();
+      return task?.description?.toLowerCase().includes(lowerCaseSearchValue);
+    }
+  };
+
+  const filterByDateRange = (task) => {
+    if (!startDateFilter || !endDateFilter) {
+      return true;
+    }
+    const taskDate = new Date(task.dueDate);
+    return taskDate >= startDateFilter && taskDate <= endDateFilter;
+  };
 
   const handleClearFilter = () => {
-    // Implement the logic to clear the filter here
-    // For example, reset selected filter and sorting order
     setSelectedFilter("");
-    setSortOrder("creationDate"); // Reset to default sorting order
+    setSortOrder("creationDate");
+    setStartDateFilter(null);
+    setEndDateFilter(null);
   };
 
   const onSelect = (task) => {
@@ -41,18 +61,30 @@ export const TaskMainPage = () => {
         onSortChange={setSortOrder}
         onClearFilter={handleClearFilter}
       />
+      <input
+        type="text"
+        className="inputSearch"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        placeholder="Buscar Tarea"
+      />
+ 
       {filteredAndSortedTasks.length > 0 && (
         <div>
-          {filteredAndSortedTasks.map((task) => (
-            <TaskCard
-              key={task.id}
-              task={task}
-              onSelectEvent={onSelect}
-              onDoubleClickEvent={onDoubleClick}
-            />
-          ))}
+          {filteredAndSortedTasks
+            .filter(filterTask)
+            .filter(filterByDateRange)
+            .map((task) => (
+              <TaskCard
+                key={task.id}
+                task={task}
+                onSelectEvent={onSelect}
+                onDoubleClickEvent={onDoubleClick}
+              />
+            ))}
         </div>
       )}
+
       <AddTaskCard />
       <TaskModal />
     </>
