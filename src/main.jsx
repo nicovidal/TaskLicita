b0b22114ec756bd218b11ce79e66a1a327315cf5
nicovidal <nico.vidal.m@gmail.com>
2024-08-11 20:22:13 -0400

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./styles.css";
-import { TaskApp } from "./taskApp.jsx";
+import { TaskApp } from "./TaskApp.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
